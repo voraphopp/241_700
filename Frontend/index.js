612f8db117e3dca09213756fefeca1b03db97b1d
voraphopp <voraphop.p@ku.th>
2026-03-06 12:12:54 +0700

    Add files via upload

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,12 +1,36 @@
+const validateData = (userData) => {
+    let errors = [];
+    if (!userData.firstName) {
+        errors.push('กรุณากรอกชื่อ');
+    }
+    if (!userData.lastName) {
+        errors.push('กรุณากรอกนามสกุล');
+    }
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุ');
+    }
+if (!userData.gender) {
+        errors.push('กรุณาเลือกเพศ');
+    }
+if (!userData.interests) {
+        errors.push('กรุณาเลือกงานอดิเรก');
+    }
+    if (!userData.description) {
+        errors.push('กรุณากรอกคำอธิบาย');
+    }
+    return errors;
+}
 
-function submitData () {
+const submitData = async () => {
     let firstNameDOM = document.querySelector('input[name=firstname]');
     let lastNameDOM = document.querySelector('input[name=lastname]');
     let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
+    let genderDOM = document.querySelector('input[name=gender]:checked') || {};
+    let interestDOMs = document.querySelectorAll('input[name=interests]:checked') || {};
     let descriptionDOM = document.querySelector('textarea[name=description]');
 
+    let messageDOM = document.getElementById('message');
+try{
     let interest = ''
     for (let i = 0; i < interestDOMs.length; i++) {
         interest += interestDOMs[i].value 
@@ -21,7 +45,39 @@ function submitData () {
         age: ageDOM.value,
         gender: genderDOM.value,
         description: descriptionDOM.value,
-        interest: interest
+        interests: interest
     }
-     console.log('submitData', userData);
+    console.log('submitData', userData);
+
+    const errors = validateData(userData);
+    if (errors.length > 0) {
+        throw {
+            message: 'กรอกข้อมูลให้ครบถ้วน',
+            errors: errors
+        }
+    }
+
+        const response = await axios.post('http://localhost:8000/users', userData);
+        console.log('response', response);
+        messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ';
+        messageDOM.className = 'message success';
+    } catch (error) {
+        console.log('error message:', error.message);
+        console.log('error details:', error.errors);
+      //  if (error.response) {
+      //      console.log('Error response:', error.response.data);
+      //  }
+        let htmlData = '<div>'
+        htmlData += `<p>${error.message}</p>`;
+        htmlData += '<ul>';
+        for (let i = 0; i < error.errors.length; i++) {
+            htmlData += `<li>${error.errors[i]}</li>`;
+        }
+        htmlData += '</ul>';
+        htmlData += '</div>';
+
+
+        messageDOM.innerHTML = htmlData;
+        messageDOM.className = 'message danger';
+    }
 }
