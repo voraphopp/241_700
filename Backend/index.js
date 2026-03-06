ba0db479d313104e6507d9f74d3c29e8406f133f
voraphopp <voraphop.p@ku.th>
2026-03-06 12:13:18 +0700

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
