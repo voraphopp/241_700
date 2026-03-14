42fb191d5e74134abf4c17624b8392edb67dba8d
voraphopp <voraphop.p@ku.th>
2026-03-14 11:34:02 +0700

    Add files via upload

--- Frontend/user.js
+++ Frontend/user.js
@@ -1,20 +1,18 @@
-//1. load user ทั้งหมด จาก api http://localhost:8000/users
-///2. นำ user ที่ได้มาแสดงในหน้าเว็บ ใน HTML
 const BASE_URL = 'http://localhost:8000';
 window.onload = async () => {
     await loadData();
 }
 
 const loadData = async () => {
-    const response = await axios.get(`${BASE_URL}/users`); // ` กด alt = 96
+    const response = await axios.get(`${BASE_URL}/users`); 
     console.log(response.data);
     const userDOM = document.getElementById('user');
     let htmlData = '<div>'
     for(let i = 0; i < response.data.length; i++) {
         let user = response.data[i];
         htmlData += `<div>
-        ${user.id} ${user.firstname} ${user.lastname}
-        <button>Edit</button>
+        ${user.id} ${user.firstname} ${user.lastname} ${user.gender}
+        <a href="index.html?id=${user.id}"><button>Edit</button></a>
         <button class='delete' data-id='${user.id}'>Delete</button>
         </div>`;
     }
@@ -25,9 +23,9 @@ const loadData = async () => {
     for(let i = 0; i < deleteDOMs.length; i++) {
         deleteDOMs[i].addEventListener('click', async (event) => {
             //ดึง id ของ user ที่ต้องการลบจาก data-id attribute
-            const userId = event.target.getAttribute('data-id');
+            const id = event.target.getAttribute('data-id');
             try {
-                await axios.delete(`${BASE_URL}/users/${userId}`);
+                await axios.delete(`${BASE_URL}/users/${id}`);
                 //http://localhost:8000/users/1
                 loadData(); //โหลดข้อมูลใหม่หลังจากลบเสร็จ
             } catch (error) {
